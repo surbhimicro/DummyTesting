24a8fc214be27e57b2f4548223598b86dd5ded50
Surbhi Mittal <surmitta@microsoft.com>
2019-08-07 14:40:11 +0530

    remove directory

--- otto-api-rks/src/main/java/com/blk/otto/pf/rks/service/IUserProfileService.java
+++
@@ -1,8 +0,0 @@
-package com.blk.otto.pf.rks.service;
-
-import com.blk.otto.pf.rks.common.IOperations;
-import com.blk.otto.pf.rks.entity.UserProfile;
-
-public interface IUserProfileService extends IOperations<UserProfile, String> {
-	
-}
